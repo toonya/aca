5cbf5d3cd6598348769ebeb86af54ebe84b2dcaa
toonya <msc5762@me.com>
2014-11-20 22:45:53 +0800

    init.

---
+++ header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html class="no-js">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title><?php wp_title(); ?></title>
+        <!-- <link rel="shortcut icon" href="<?php echo get_template_directory_uri();?>/favicon.ico"> -->
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <link rel="stylesheet" href="<?php echo get_template_directory_uri()?>/css/font-awesome.min.css">
+        <link rel="stylesheet" href="<?php echo get_template_directory_uri()?>/css/bootstrap.min.css">
+        <link rel="stylesheet" href="<?php echo get_template_directory_uri()?>/css/bootstrap-theme.min.css">
+        <link rel="stylesheet" href="<?php echo get_template_directory_uri()?>/style.css">
+
+        <?php wp_head()?>
+    </head>
+    <body <?php if(wp_is_mobile()) echo 'class="mobile"' ?>>
+       <header>
+           
+       </header> <!-- /.header -->
+

044af847fa491d7b9ac30fbc3e716289993c9550
toonya <msc5762@me.com>
2014-11-20 23:08:15 +0800

    init. front-page template.

--- footer.php
+++ footer.php
@@ -1,4 +1,27 @@
         
+		<footer>
+		    <div class="container">
+		        <div class="row">
+		            <div class="col-sm-4">
+		                <div class="title">聯繫我們</div>
+		                <div class="contact">
+		                    <p>Tel: 1300 222 667</p>            
+		                    <p>D**: (02) 8262 8566</p>
+		                    <p>Mob: 0414 573 938</p>          
+		                    <p>Fax: (02) 9287 7773</p>
+		                </div>
+		                <div class="bottom">
+		                    <p>Email: raymond@acahomeloans.com.au</p>
+		                    <p>Add: Level 10, 265 Castlereagh ST Sydney 2000</p>
+		                </div>
+		                <div class="copyright">
+		                    Copyright © 2014 ACA. All Rights Reserved.
+		                </div>
+		            </div>
+		        </div>
+		    </div>
+		</footer> <!-- /. end of footer -->
+
         <script src="<?php echo get_template_directory_uri()?>/js/vendor/jquery-1.11.0.min.js"></script>
         <script src="<?php echo get_template_directory_uri()?>/js/vendor/bootstrap.min.js"></script>
         <script src="<?php echo get_template_directory_uri()?>/js/main.js"></script>
--- header.php
+++ header.php
@@ -4,7 +4,6 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
         <title><?php wp_title(); ?></title>
-        <!-- <link rel="shortcut icon" href="<?php echo get_template_directory_uri();?>/favicon.ico"> -->
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         
@@ -17,6 +16,50 @@
     </head>
     <body <?php if(wp_is_mobile()) echo 'class="mobile"' ?>>
        <header>
-           
-       </header> <!-- /.header -->
+            <div class="container .row">
+                <div class="col-xs-6">
+                    <img src="<?php echo get_template_directory_uri(); ?>/img/logo.png" alt="" class="logo">
+                </div> <!-- /. end of left header -->
+                <div class="col-xs-6 text-right">
+                    <div class="language">
+                        <a href="">中文</a>
+                         / 
+                        <a href="">English</a>
+                    </div> <!-- /. end of language -->
+                    <!-- <div class="contact">
+                        <a href="" title="期待您的来电">
+                            <img src="<?php echo get_template_directory_uri(); ?>/img/telephone.png" alt=""> 1300 222 667
+                        </a>
+                    </div> --> 
+                    <!-- /. end of phone call -->
+                </div> <!-- /. end of right header -->
+            </div>
+        </header> <!-- /.header -->
+
+        <nav class="navbar" role="navigation">
+            <div class="container">
+                <div class="col-sm-8">
+                    <ul class="menu-list">
+                        <li><a href="#">首  頁</a></li>
+                        <li><a href="#">關  于</a></li>
+                        <li><a href="#">業務範圍</a></li>
+                        <li><a href="#">新  聞</a></li>
+                        <li><a href="#">聯繫我們</a></li>
+                        <li><a href="#">表  格</a></li>
+                    </ul>
+
+                </div>
+                <div class="search col-sm-4">
+                    <div class="contact">
+                        <a href="" title="期待您的来电">
+                            <img src="<?php echo get_template_directory_uri(); ?>/img/telephone.png" alt=""> 1300 222 667
+                        </a>
+                    </div>
+                    <!-- <form action="">
+                        <input type="text" placeholder="全站搜索">
+                        <button type="submit"></button>
+                    </form> -->
+                </div>
+            </div>
+        </nav>
 

1788a368d1174a786992f4fab04b53630951621a
toonya <msc5762@me.com>
2014-11-30 11:00:00 +0800

    Revert af670c7..ce0bf7d
    This rolls back to commit af670c7429aae3e3af0770e731d68aee4fe1778e.

--- header.php
+++ header.php
@@ -22,43 +22,28 @@
         ?>
     ">
         <header>
-        <div class ="right">
-           <!-- <div class="container .row"> 为什么要这个DIV-->
-                 <div class="contact">
-                        <a href="" title="期待您的来电">
-                            <img src="<?php echo get_template_directory_uri(); ?>/img/telephone.png" alt=""> 1300 222 667
-                        </a>
-                    </div>
-                  <div class="language">
+            <div class="container .row">
+                <div class="col-xs-6">
+                    <img src="<?php echo get_template_directory_uri(); ?>/img/logo.png" alt="" class="logo">
+                </div> <!-- /. end of left header -->
+                <div class="col-xs-6 text-right">
+                    <div class="language">
                         <!-- <a href="">中文</a>
                          / 
                         <a href="">English</a> -->
 
                         <?php if(get_language_list()) echo get_language_list(); ?>
                     </div> <!-- /. end of language -->
-                    </div> <!-- /. end of right header -->
-           
-
-            <!-- <div class="container .row">
-                <div class="col-xs-6">
-                    <img src="<?php echo get_template_directory_uri(); ?>/img/logo.png" alt="" class="logo">
-                </div>  /. end of left header    和菜单合并为一行，28日晚改-->
-                <div class="col-xs-6 text-right">
-                    
                     <!-- <div class="contact">
-                        <a href="" title="Please contact us">
+                        <a href="" title="期待您的来电">
                             <img src="<?php echo get_template_directory_uri(); ?>/img/telephone.png" alt=""> 1300 222 667
                         </a>
                     </div> --> 
                     <!-- /. end of phone call -->
-                
-            </div> 
-</header> <!-- /.header -->
-        
+                </div> <!-- /. end of right header -->
+            </div>
+        </header> <!-- /.header -->
         <nav class="navbar" role="navigation">
-            <div class="toplogo">
-                    <img src="<?php echo get_template_directory_uri(); ?>/img/logo.png" alt="" class="logo">
-              </div>
             <div class="container">
                 <div class="col-sm-8">
                     <!-- <ul class="menu-list">
@@ -97,7 +82,11 @@
                     ?>
                 </div>
                 <div class="search col-sm-4">
-                    
+                    <div class="contact">
+                        <a href="" title="期待您的来电">
+                            <img src="<?php echo get_template_directory_uri(); ?>/img/telephone.png" alt=""> 1300 222 667
+                        </a>
+                    </div>
                     <!-- <form action="">
                         <input type="text" placeholder="全站搜索">
                         <button type="submit"></button>

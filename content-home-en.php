65fd47bb14cd17cf21ba2be586624045ddf76007
toonya <msc5762@me.com>
2014-12-01 11:01:21 +0800

    header 2.

--- content-home-en.php
+++ content-home-en.php
@@ -1,52 +1,50 @@
 <div class="main home">
-    <div class="container">
-        <section class="slide">
-         <!-- Modal -->
-            <div id="home-slide" class="carousel slide" data-ride="carousel">
-                 <!-- Wrapper for slides -->
-                <div class="carousel-inner">
-                    <?php  
-                        $ty_img_list = get_option('ty_imglisten');
-                        foreach ($ty_img_list as $key => $item) :;?>
-                            <div class="item <?php if( $key == 0 ) echo ' active';?>" style="background-image:url(<?php echo $item['imgurl']; ?>)">
-                                <div class="carousel-caption right">
-                                    <div class="carousel-title">
-                                        <?php echo $item['title']; ?>
-                                    </div>
-                                    <div class="row">
-                                        <div class="col-sm-6">
-                                            <div class="year">
-                                                <?php echo $item['year1']; ?>Year Lorem.
-                                            </div>
-                                            <div class="rate">
-                                                 <?php echo $item['rate1']; ?><span>%</span>
-                                            </div>
-                                        </div>
-                                        <div class="col-sm-6">
-                                            <div class="year">
-                                                <?php echo $item['year2']; ?>Year Lorem.
-                                            </div>
-                                            <div class="rate">
-                                                 <?php echo $item['rate2'] ?><span>%</span>
-                                            </div>
-                                        </div>
-                                    </div>
-
-                                    <div class="describe"><?php echo $item['describe']; ?></div>
-                                </div>
-                            </div><!-- /. end of item. -->
-                        <?php endforeach;
-                    ?>
-                 </div>
-             
-                 <!-- Controls -->
-                <a class="left carousel-control" href="#home-slide" role="button" data-slide="prev">
-                </a>
-                <a class="right carousel-control" href="#home-slide" role="button" data-slide="next">
-                </a>
-            </div>
-        </section>
-    </div> <!-- /. end of slide -->
+    <section class="slide">
+      <!-- Modal -->
+         <div id="home-slide" class="carousel slide" data-ride="carousel">
+              <!-- Wrapper for slides -->
+             <div class="carousel-inner">
+                 <?php  
+                     $ty_img_list = get_option('ty_imglisten');
+                     foreach ($ty_img_list as $key => $item) :;?>
+                         <div class="item <?php if( $key == 0 ) echo ' active';?>" style="background-image:url(<?php echo $item['imgurl']; ?>)">
+                             <div class="carousel-caption right">
+                                 <div class="carousel-title">
+                                     <?php echo $item['title']; ?>
+                                 </div>
+                                 <div class="row">
+                                     <div class="col-sm-6">
+                                         <div class="year">
+                                             <?php echo $item['year1']; ?>Year Lorem.
+                                         </div>
+                                         <div class="rate">
+                                              <?php echo $item['rate1']; ?><span>%</span>
+                                         </div>
+                                     </div>
+                                     <div class="col-sm-6">
+                                         <div class="year">
+                                             <?php echo $item['year2']; ?>Year Lorem.
+                                         </div>
+                                         <div class="rate">
+                                              <?php echo $item['rate2'] ?><span>%</span>
+                                         </div>
+                                     </div>
+                                 </div>
+     
+                                 <div class="describe"><?php echo $item['describe']; ?></div>
+                             </div>
+                         </div><!-- /. end of item. -->
+                     <?php endforeach;
+                 ?>
+              </div>
+          
+              <!-- Controls -->
+             <a class="left carousel-control" href="#home-slide" role="button" data-slide="prev">
+             </a>
+             <a class="right carousel-control" href="#home-slide" role="button" data-slide="next">
+             </a>
+         </div>
+     </section> <!-- /. end of slide -->
 
     <section class="honour">
         <div class="container">
--- header.php
+++ header.php
@@ -22,76 +22,59 @@
         ?>
     ">
         <header>
-            <div class="container .row">
-                <div class="col-xs-6">
-                    <img src="<?php echo get_template_directory_uri(); ?>/img/logo.png" alt="" class="logo">
-                </div> <!-- /. end of left header -->
-                <div class="col-xs-6 text-right">
-                    <div class="language">
-                        <!-- <a href="">中文</a>
-                         / 
-                        <a href="">English</a> -->
-
-                        <?php if(get_language_list()) echo get_language_list(); ?>
-                    </div> <!-- /. end of language -->
-                    <!-- <div class="contact">
-                        <a href="" title="期待您的来电">
-                            <img src="<?php echo get_template_directory_uri(); ?>/img/telephone.png" alt=""> 1300 222 667
-                        </a>
-                    </div> --> 
-                    <!-- /. end of phone call -->
-                </div> <!-- /. end of right header -->
+            <div class="container">
+                <div class="col-xs-12 text-right top-bar">
+                     <div class="language">
+                         <!-- <a href="">中文</a>
+                          / 
+                         <a href="">English</a> -->
+                 
+                         <?php if(get_language_list()) echo get_language_list(); ?>
+                     </div> <!-- /. end of language -->
+                     <div class="contact">
+                         <a href="" title="期待您的来电">
+                             <span class="glyphicon glyphicon-earphone"></span> 1300 222 667
+                         </a>
+                     </div> 
+                     <!-- /. end of phone call -->
+                 </div> <!-- /. end of right header -->
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-5">
+                        <img src="<?php echo get_template_directory_uri(); ?>/img/logo.png" alt="" class="logo">
+                    </div> <!-- /. end of left header -->
+                    <div class="col-xs-7 nav-wrapper">
+                        <nav class="navbar" role="navigation">
+                            <?php 
+                                $locatioin = '';
+                                if(wp_is_mobile()) $locatioin = 'mobile-menu';
+                                else $locatioin = 'header-menu';
+                                
+                                $head_navigation = array(
+                                    'theme_location'  => $locatioin,
+                                    'menu'            => '',
+                                    'container'       => false,
+                                    'container_class' => '',
+                                    'container_id'    => '',
+                                    'menu_class'      => 'menu-list',
+                                    'menu_id'         => '',
+                                    'echo'            => true,
+                                    'fallback_cb'     => false,
+                                    'before'          => '',
+                                    'after'           => '',
+                                    'link_before'     => '',
+                                    'link_after'      => '',
+                                    'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+                                    'depth'           => 0,
+                                    'walker'          => new Bootsrap_basic(),
+                                );
+                                            
+                                wp_nav_menu( $head_navigation );
+                            ?>
+                        </nav>
+                    </div>
+                </div>
             </div>
         </header> <!-- /.header -->
-        <nav class="navbar" role="navigation">
-            <div class="container">
-                <div class="col-sm-8">
-                    <!-- <ul class="menu-list">
-                        <li><a href="#">首  頁</a></li>
-                        <li><a href="#">關  于</a></li>
-                        <li><a href="#">業務範圍</a></li>
-                        <li><a href="#">新  聞</a></li>
-                        <li><a href="#">聯繫我們</a></li>
-                        <li><a href="#">表  格</a></li>
-                    </ul> -->
-                    <?php 
-                        $locatioin = '';
-                        if(wp_is_mobile()) $locatioin = 'mobile-menu';
-                        else $locatioin = 'header-menu';
-                        
-                        $head_navigation = array(
-                            'theme_location'  => $locatioin,
-                            'menu'            => '',
-                            'container'       => false,
-                            'container_class' => '',
-                            'container_id'    => '',
-                            'menu_class'      => 'menu-list',
-                            'menu_id'         => '',
-                            'echo'            => true,
-                            'fallback_cb'     => false,
-                            'before'          => '',
-                            'after'           => '',
-                            'link_before'     => '',
-                            'link_after'      => '',
-                            'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
-                            'depth'           => 0,
-                            'walker'          => new Bootsrap_basic(),
-                        );
-
-                        wp_nav_menu( $head_navigation );
-                    ?>
-                </div>
-                <div class="search col-sm-4">
-                    <div class="contact">
-                        <a href="" title="期待您的来电">
-                            <img src="<?php echo get_template_directory_uri(); ?>/img/telephone.png" alt=""> 1300 222 667
-                        </a>
-                    </div>
-                    <!-- <form action="">
-                        <input type="text" placeholder="全站搜索">
-                        <button type="submit"></button>
-                    </form> -->
-                </div>
-            </div>
-        </nav>
 

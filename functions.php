90ba18105a271a31f0ffcf17243cdd424092ccfa
toonya <msc5762@me.com>
2014-12-04 14:20:58 +0800

    disable update

--- functions.php
+++ functions.php
@@ -128,3 +128,16 @@ function get_language_list() {
 		return false;
 	}
 }
+
+// ----------------------------------------
+// ! disable update
+// ----------------------------------------
+function hide_update_notice_to_all_but_admin_users()
+{
+    // if (!current_user_can('update_core')) {
+    //     remove_action( 'admin_notices', 'update_nag', 10);
+    // }
+
+    remove_action( 'admin_notices', 'update_nag', 3);
+}
+add_action( 'admin_head', 'hide_update_notice_to_all_but_admin_users', 1 );
